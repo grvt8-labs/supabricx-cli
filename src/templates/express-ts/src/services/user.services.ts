fe294f7ac0c00cfe911dc32bdba3a831227ab36c
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-09-20 13:14:51 +0100

    Refactoring user logic

--- src/templates/express-ts/src/services/user.services.ts
+++ src/templates/express-ts/src/services/user.services.ts
@@ -1,12 +1,11 @@
 import { CreateUserDto } from "../dtos/user.dto.js";
 import { User } from "@prisma/client";
 
-
-export interface UserService {
+export interface IUserService {
   getAllUsers(): Promise<User[]>;
   getUserById(id: number): Promise<User | null>;
   createUser(dto: CreateUserDto): Promise<User>;
-  updateUser(id: number, data: Partial<CreateUserDTO>): Promise<User>;
+  updateUser(id: number, data: Partial<CreateUserDto>): Promise<User>;
   deleteUser(id: number): Promise<void>;
-  profile(id: number): Promise<Omit<User, "password">>
+  profile(id: number): Promise<Omit<User, "password"> | null>;
 }
